b2405a20c0668358af61bb80897801a72ed4a0d2
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2021-08-22 14:07:27 +0530

    Add files via upload

---
+++ build_install_module.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python setup.py build_ext --force --user
+python setup.py install --user
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize("CASSPER_tf2.py"),
+)
+
+# Compile:
+# python setup.py build_ext --inplace
+# or
+# python setup.py build_ext --force --user
+# python setup.py install --user
+
