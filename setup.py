6ac43378eb91ca52e8a8a08d9f330e61b028867a
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2022-03-02 11:45:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize("CASSPER2.py"),
-)
-
-# Compile:
-# python setup.py build_ext --inplace
-# or
-# python setup.py build_ext --force --user
-# python setup.py install --user
+ python setup.py build_ext --inplace
 
