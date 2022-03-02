f12101b2e8d487eefcef41cad96ff60efc4edb63
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2022-03-02 12:20:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
 
     url="https://github.com/sajeethphilip/CASSPER2",
 
-    packages=setuptools.find_packages("CASSPER2.py"),
+    packages=setuptools.find_packages(include=['CASSPER2*',**']),
 
     classifiers=[
 
