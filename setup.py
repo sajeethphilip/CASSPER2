1a208830b1e9d6a19286120d8866868ca67deff4
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2022-03-02 11:51:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,40 @@
-from distutils.core import setup
- python setup.py build_ext --inplace
+import setuptools
 
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="sajeethphilip", # Replace with your username
+
+    version="1.0.0",
+
+    author="Ninan Sajeeth Philip",
+
+    author_email="nsp@airis4d.com",
+
+    description="CASSPER-v2 install",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/sajeethphilip/CASSPER2",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.6',
+
+)
+ 
